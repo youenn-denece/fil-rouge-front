fc871aadbf4f4fa265b28fad43eceea9b6f8ec0f
lucierou <lucie.rouault@live.fr>
2023-01-16 09:49:50 +0100

    dashboard components

---
+++ it-training/src/app/dashboard-index/dashboard-search/dashboard-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-search',
+  templateUrl: './dashboard-search.component.html',
+  styleUrls: ['./dashboard-search.component.scss']
+})
+export class DashboardSearchComponent {
+
+}
