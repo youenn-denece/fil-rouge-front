99d0a0e782229244667af173d5413e664dbfc57d
Nathan-bioinfo <nathan.caceres@laposte.net>
2023-01-16 11:30:48 +0100

    connexion component link to navbar

--- it-training/src/app/app.module.ts
+++ it-training/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 import { HomeComponent } from './home/home.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
